f8691d08c4ededaabb46a3048abf0d1693eb7984
Jhon Rey Ranario <jr.ran90@gmail.com>
2018-10-19 04:53:18 +0800

    update pending menu

--- src/components/PendingUser.vue
+++ src/components/PendingUser.vue
@@ -3,29 +3,29 @@
 	<div>
 		<h2>Pending Users</h2>
 		<div class="row">
-			<div class="col-md-8">
+			<div class="col-md-6">
 				<table class="table table-hover table-striped">
 					<thead>
 						<tr>
-							<th>Name</th>
-							<th>Hp</th>
-							<th>Date Registered</th>
-							<th></th>
+							<th colspan="2">Phone Number</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr v-for="(data, index) in users">
-							<td class="col-4">{{ data.Name }}</td>
-							<td class="col-3">{{ data.Hp }}</td>
-							<!-- <td class="col-3">{{ data.created_at | moment("YYYY-MM-D h:mm a") }}</td> -->
-							<!-- <td class="col-2 text-center">
-								<a href @click.prevent="openModal(data, index)">
-									<i class="nc-icon nc-zoom-split"></i> View
-								</a>
-							</td> -->
+							<td>{{ data.Hp }}</td>
+							<td class="text-right">
+								<ul class="list-inline m-0">
+									<li class="list-inline-item">
+										<a href @click.prevent="activate(data.Hp, index)">Approve</a>
+									</li>
+									<li class="list-inline-item">
+										<a href @click.prevent="openModalUserDetails(data)">View details</a>
+									</li>
+								</ul>
+							</td>
 						</tr>
 						<tr v-if="users.length===0">
-							<td colspan="3">No Record found!</td>
+							<td colspan="2">No Record found!</td>
 						</tr>
 					</tbody>
 				</table>
@@ -40,91 +40,73 @@
 				<div class="col-md-8">
 					<table class="table table-bordered table-sm">
 						<tr>
-							<td class="table-info">Name</td>
-							<td class="table-secondary">{{ `${modalDataUser.firstname} ${modalDataUser.lastname}` }}</td>
-						</tr>
-						<tr>
-							<td class="table-info">Gender</td>
-							<td class="table-secondary">{{ modalDataUser.gender }}</td>
-						</tr>
-						<tr>
-							<td class="table-info">Date of Birth</td>
-							<td class="table-secondary">{{ modalDataUser.dob }}</td>
-						</tr>
-						<tr>
-							<td class="table-info">Mobile No.</td>
-							<td class="table-secondary">{{ modalDataUser.mobile }}</td>
+							<td class="table-info">Alamat</td>
+							<td class="table-secondary">{{ userDetails.alamat }}</td>
 						</tr>
 						<tr>
 							<td class="table-info">E-mail</td>
-							<td class="table-secondary">{{ modalDataUser.email }}</td>
+							<td class="table-secondary">{{ userDetails.email }}</td>
 						</tr>
-						<tr>
-							<td class="table-info">Address</td>
-							<td class="table-secondary">{{ modalDataUser.address }}</td>
-						</tr>
-
-						<tr><th class="table-success" colspan="2">Emergency Number</th></tr>
-
 						<tr>
 							<td class="table-info">Name</td>
-							<td class="table-secondary">{{ modalDataUser.en_name }}</td>
+							<td class="table-secondary">{{ `${userDetails.fname} ${userDetails.lname}` }}</td>
 						</tr>
 						<tr>
-							<td class="table-info">Emergency Contact</td>
-							<td class="table-secondary">{{ modalDataUser.en_emergency_contact }}</td>
+							<td class="table-info">Tanggal Lahir</td>
+							<td class="table-secondary"></td>
 						</tr>
 						<tr>
-							<td class="table-info">Mobile No.</td>
-							<td class="table-secondary">{{ modalDataUser.en_mobile_no }}</td>
+							<td class="table-info">No. KTP</td>
+							<td class="table-secondary">{{ userDetails.idcard }}</td>
 						</tr>
 						<tr>
-							<td class="table-info">Income</td>
-							<td class="table-secondary">{{ modalDataUser.en_income }}</td>
-						</tr>
-
-						<tr><th class="table-success" colspan="2">Personal Data</th></tr>
-
-						<tr>
-							<td class="table-info">Industry</td>
-							<td class="table-secondary">{{ modalDataUser.pd_industry }}</td>
+							<td class="table-info">Bidang Kerja</td>
+							<td class="table-secondary"></td>
 						</tr>
 						<tr>
-							<td class="table-info">Work</td>
-							<td class="table-secondary">{{ modalDataUser.pd_work }}</td>
+							<td class="table-info">Pekerjaan</td>
+							<td class="table-secondary">{{ userDetails.pekerjaan}}</td>
 						</tr>
 						<tr>
-							<td class="table-info">Education</td>
-							<td class="table-secondary">{{ modalDataUser.pd_education }}</td>
+							<td class="table-info">Pendidikan</td>
+							<td class="table-secondary">{{ userDetails.pendidikan }}</td>
 						</tr>
 						<tr>
-							<td class="table-info">Income</td>
-							<td class="table-secondary">{{ modalDataUser.pd_income }}</td>
+							<td class="table-info">Penghasilan</td>
+							<td class="table-secondary">{{ userDetails.penghasilan }}</td>
+						</tr>
+						<tr>
+							<td class="table-info">No. NPWP</td>
+							<td class="table-secondary">{{ userDetails.npwp }}</td>
+						</tr>
+						<tr>
+							<td class="table-info">Email Verifikasi</td>
+							<td class="table-secondary">{{ ((userDetails.emailverified==0)?'Belum':'Sudah') }}</td>
 						</tr>
 					</table>
-					<div>
-						<div class="form-group row">
-							<label for="inputAssignCredit" class="col-sm-4 col-form-label">Assign Credit</label>
-							<div class="col-sm-8">
-								<input type="number" class="form-control text-right" id="inputAssignCredit" placeholder="Assign Credit" v-model="inputCredit">
-							</div>
-						</div>
-					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="card">
 						<div class="card-body">
-							<figure class="figure">
-								<img :src="modalDataUser.photo_identity_card" class="figure-img img-fluid rounded" alt="identification card photo">
-								<figcaption class="figure-caption">Picture of the Identification card</figcaption>
+							<figure class="figure m-0">
+								<img :src="userImageProfile" class="figure-img img-fluid rounded" alt="profile picture">
+								<figcaption class="figure-caption">Profile Picture</figcaption>
 							</figure>
 						</div>
 					</div>
 					<div class="card">
 						<div class="card-body">
-							<figure class="figure">
-								<img :src="modalDataUser.photo_holding_the_card" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-								<figcaption class="figure-caption">Picture holding the Identification card</figcaption>
+							<figure class="figure m-0">
+								<img :src="userImageKtp" class="figure-img img-fluid rounded" alt="ktp">
+								<figcaption class="figure-caption">KTP</figcaption>
+							</figure>
+						</div>
+					</div>
+					<div class="card">
+						<div class="card-body">
+							<figure class="figure m-0">
+								<img :src="userDetails.selfie" class="figure-img img-fluid rounded" alt="selfie with ktp">
+								<figcaption class="figure-caption">Selfie with KTP</figcaption>
 							</figure>
 						</div>
 					</div>
@@ -132,10 +114,9 @@
 			</div>
 			<div slot="modal-footer">
 				<button class="btn btn-lg btn-secondary" @click="modalUserShow = false">Cancel</button>
-				<button class="btn btn-lg btn-danger mr-2 ml-2">Reject</button>
-				<button class="btn btn-lg btn-success" @click="changeStatus(modalDataUser._id, modalDataUser.index)">Save</button>
 			</div>
 		</b-modal>
+				<!-- <button class="btn btn-lg btn-success" @click="changeStatus(modalDataUser._id, modalDataUser.index)">Save</button> -->
 	</div>
 
 </template>
@@ -148,46 +129,41 @@ export default {
 		return {
 			modalUserShow: false,
 			users: {},
-			modalDataUser: {},
-			inputCredit: 0
+			inputCredit: 0,
+			userDetails: {},
+			userImageProfile: '',
+			userImageKtp: '',
+			userImageSelfieWithKtp: '',
 		}
 	},
 	created() {
 		let vm = this
 		axios.get(`${process.env.VUE_APP_API_URL}/users/pending`, {
 			headers: { Authorization: "Bearer " + localStorage.getItem('auth_token') }
-		}).then(res => {
-			vm.users = res.data
-			console.log('pending', res.data)
-		})
+		}).then(res => vm.users = res.data)
 	},
 	methods: {
-		openModal(user, index) {
+		openModalUserDetails(user, index) {
 			let vm = this
+
+			// reset every time the modal is clicked
+			vm.userDetails = {}
+			vm.userImageKtp = ''
+			vm.userImageProfile = ''
+			vm.userImageSelfieWithKtp = ''
+
 			vm.modalUserShow = true
-			vm.modalDataUser = {
-				index: index,
-				_id: user._id,
-				firstname: user.firstname,
-				lastname: user.lastname,
-				gender: user.gender,
-				dob: user.dob,
-				mobile: user.mobile,
-				email: user.email,
-				address: user.address,
-				photo_identity_card: `${process.env.VUE_APP_API_URL}/${user.photo.identity_card}`,
-				photo_holding_the_card: `${process.env.VUE_APP_API_URL}/${user.photo.holding_the_card}`,
-				// Emergency Number
-				en_emergency_contact: ((user.emergency != undefined) ? user.emergency_number.emergency_contact : '---'),
-				en_income: ((user.emergency != undefined) ? user.emergency_number.income : '---'),
-				en_mobile_no: ((user.emergency != undefined) ? user.emergency_number.mobile_no : '---'),
-				en_name: ((user.emergency != undefined) ? user.emergency_number.name : '---'),
-				// Personal Data
-				pd_industry: ((user.personal_data != undefined) ? user.personal_data.industry : '---'),
-				pd_work: ((user.personal_data != undefined) ? user.personal_data.work : '---'),
-				pd_education: ((user.personal_data != undefined) ? user.personal_data.education : '---'),
-				pd_income: ((user.personal_data != undefined) ? user.personal_data.income : '---')
-			}
+			axios.get(`${process.env.VUE_APP_API_URL}/users/${user.Hp}`, {
+				headers: { Authorization: "Bearer " + localStorage.getItem('auth_token') }
+			}).then(res => {
+				vm.userDetails = res.data[0]
+				let parseImage = JSON.parse(res.data[0].ktp) // lol this, I need to parse???
+
+				for (let x in parseImage) {
+					if (x==='ktp') vm.userImageKtp = parseImage[x]
+					if (x==='photo') vm.userImageProfile = parseImage[x]
+				}
+			})
 		},
 
 		/**
@@ -196,7 +172,7 @@ export default {
 		 * @param  {integer} id    User Id
 		 * @param  {integer} index Index, Key
 		 */
-		changeStatus(id, index) {
+		/*changeStatus(id, index) {
 			let vm = this
 			let dataInput = {
 				user_id: id,
@@ -210,15 +186,27 @@ export default {
 					vm.modalUserShow = false
 				})
 			}
-		},
-		activate(id) {
-			/*if (confirm("You really want to activate this account?")) {
-				// axios.put(`http://127.0.0.1:8080/users/${id}/assign-credit`, { status: true }).then(res => {
-					// alert(res.data.msg)
-				// 	this.modalUserShow = false
-				// 	axios.get('http://127.0.0.1:8080/users?status=0').then(res2 => vm.users = res2.data.data)
-				// })
-			}*/
+		},*/
+
+		/**
+		 * Activate user
+		 * 
+		 * @param  int hp Phone Number
+		 * @param  int index
+		 */
+		activate(hp, index) {
+			let vm = this
+			let dataInput = {
+				credit: 0,
+				activated: 2
+			}
+			if (confirm("Activate this account?")) {
+				axios.post(`${process.env.VUE_APP_API_URL}/users/${hp}/assign-credit`, dataInput).then((res) => {
+					alert("Successfully Activated!")
+					vm.users.splice(index, 1)
+					console.log(res.data)
+				})
+			}
 		}
 	}
 }

f0d8418c53615a2cd8af40ee3101e8bad5db3c3a
Dhonald Johannes C.H. Sihotang <dhonaldjohannes@gmail.com>
2018-11-28 11:58:37 +0700

    Add some menu and integrate AllPromotion Menut with API

---
+++ src/components/AllPromotion.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2>All Promotion at Merchant EmpatKali</h2>
+    <table class="table table-hover table-striped">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Period</th>
+          <th>Diskon</th>
+          <th>Detail</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in allPromotion">
+          <td>{{ data.No }}</td>
+          <td style="width: 20%">{{ data.title }}</td>
+          <td>{{ data.author }}</td>
+          <td style="width: 20%">{{ data.period }}</td>
+          <td>{{ data.diskon }}%</td>
+          <td>{{ data.detail }}</td>
+        </tr>
+        <tr v-if="allPromotion.length===0">
+          <td colspan="4">No transaction record!</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      allPromotion: {},
+      transactionDetail: {},
+      transactionDetailHeader: "",
+      modalTransactionDetail: false
+    };
+  },
+  created() {
+    let vm = this;
+    axios
+      .get(`${process.env.VUE_APP_API_URL}/promo`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("auth_token")
+        }
+      })
+      .then(res => (vm.allPromotion = res.data));
+  }
+};
+</script>

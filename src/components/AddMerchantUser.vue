47d33a50a1cce3253e54f17e91b1ed7b088d60bc
Dhonald Johannes C.H. Sihotang <dhonaldjohannes@gmail.com>
2018-11-29 16:00:54 +0700

    Adding Add merchant user function

--- src/components/AddMerchantUser.vue
+++ src/components/AddMerchantUser.vue
@@ -1,126 +1,65 @@
 <template>
-  <div>
-    <h2>Pending Users</h2>
-    <div class="row">
-      <div class="col-md-6">
-        <table class="table table-hover table-striped">
-          <thead>
-            <tr>
-              <th colspan="2">Phone Number</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(data, index) in users">
-              <td>{{ data.Hp }}</td>
-              <td class="text-right">
-                <ul class="list-inline m-0">
-                  <li class="list-inline-item">
-                    <a href @click.prevent="activate(data.Hp, index)">Approve</a>
-                  </li>
-                  <li class="list-inline-item">
-                    <a href @click.prevent="openModalUserDetails(data)">View details</a>
-                  </li>
-                </ul>
-              </td>
-            </tr>
-            <tr v-if="users.length===0">
-              <td colspan="2">No Record found!</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+  <div class="py-4">
+    <h2>
+      <font-awesome-icon icon="bullhorn"/>Add Merchant User
+      <a
+        href="#"
+        class="btn btn-primary"
+        @click.prevent="openModal('AddCategory')"
+      >Add</a>
+    </h2>
 
-    <b-modal v-model="modalUserShow" size="lg">
-      <div slot="modal-header">
-        <h4>User Detail</h4>
-      </div>
-      <div class="row">
-        <div class="col-md-8">
-          <table class="table table-bordered table-sm">
-            <tr>
-              <td class="table-info">Alamat</td>
-              <td class="table-secondary">{{ userDetails.alamat }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">E-mail</td>
-              <td class="table-secondary">{{ userDetails.email }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">Name</td>
-              <td class="table-secondary">{{ `${userDetails.fname} ${userDetails.lname}` }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">Tanggal Lahir</td>
-              <td class="table-secondary"></td>
-            </tr>
-            <tr>
-              <td class="table-info">No. KTP</td>
-              <td class="table-secondary">{{ userDetails.idcard }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">Bidang Kerja</td>
-              <td class="table-secondary"></td>
-            </tr>
-            <tr>
-              <td class="table-info">Pekerjaan</td>
-              <td class="table-secondary">{{ userDetailsPekerjaan }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">Pendidikan</td>
-              <td class="table-secondary">{{ userDetails.pendidikan }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">Penghasilan</td>
-              <td class="table-secondary">{{ userDetailsPenghasilan }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">No. NPWP</td>
-              <td class="table-secondary">{{ userDetails.npwp }}</td>
-            </tr>
-            <tr>
-              <td class="table-info">Email Verifikasi</td>
-              <td class="table-secondary">{{ ((userDetails.emailverified==0)?'Belum':'Sudah') }}</td>
-            </tr>
-          </table>
-        </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <figure class="figure m-0">
-                <img
-                  :src="userImageProfile"
-                  class="figure-img img-fluid rounded"
-                  alt="profile picture"
-                >
-                <figcaption class="figure-caption">Profile Picture</figcaption>
-              </figure>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <figure class="figure m-0">
-                <img :src="userImageKtp" class="figure-img img-fluid rounded" alt="ktp">
-                <figcaption class="figure-caption">KTP</figcaption>
-              </figure>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <figure class="figure m-0">
-                <img
-                  :src="userDetails.selfie"
-                  class="figure-img img-fluid rounded"
-                  alt="selfie with ktp"
-                >
-                <figcaption class="figure-caption">Selfie with KTP</figcaption>
-              </figure>
-            </div>
+    <!-- modals -->
+    <b-modal v-model="modalAddMerchantUser" title="Add Merchant User" @ok="addMerchUser">
+      <div>
+        <div class="form-group row">
+          <label for="inputUsername" class="col-sm-4 col-form-label">Username</label>
+          <div class="col-sm-8">
+            <input
+              type="text"
+              class="form-control"
+              id="inputUsername"
+              placeholder="Username"
+              v-model="dataAddMerchantUser.id"
+            >
           </div>
         </div>
-      </div>
-      <div slot="modal-footer">
-        <button class="btn btn-lg btn-secondary" @click="modalUserShow = false">Close</button>
+        <div class="form-group row">
+          <label for="inputpassword" class="col-sm-4 col-form-label">Password</label>
+          <div class="col-sm-8">
+            <input
+              type="password"
+              class="form-control"
+              id="inputpassword"
+              placeholder="Password"
+              v-model="dataAddMerchantUser.pass"
+            >
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="inputMerchantName" class="col-sm-4 col-form-label">Nama Merchant</label>
+          <div class="col-sm-8">
+            <input
+              type="text"
+              class="form-control"
+              id="inputMerchantName"
+              placeholder="Nama Merchant"
+              v-model="dataAddMerchantUser.name"
+            >
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="inputPrefix" class="col-sm-4 col-form-label">Prefix</label>
+          <div class="col-sm-8">
+            <input
+              type="text"
+              class="form-control"
+              id="inputPrefix"
+              placeholder="Prefix"
+              v-model="dataAddMerchantUser.prefix"
+            >
+          </div>
+        </div>
       </div>
     </b-modal>
   </div>
@@ -132,135 +71,35 @@ import axios from "axios";
 export default {
   data() {
     return {
-      modalUserShow: false,
-      users: {},
-      inputCredit: 0,
-      userDetails: {},
-      userImageProfile: "",
-      userImageKtp: "",
-      userImageSelfieWithKtp: "",
-      userDetailsPekerjaan: "",
-      userDetailsPenghasilan: ""
+      modalAddMerchantUser: false,
+      dataAddMerchantUser: {}
     };
   },
-  created() {
-    let vm = this;
-    axios
-      .get(`${process.env.VUE_APP_API_URL}/users/pending`, {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("auth_token")
-        }
-      })
-      .then(res => (vm.users = res.data));
-  },
   methods: {
-    openModalUserDetails(user, index) {
+    openModal(modal, data) {
       let vm = this;
-
-      // reset every time the modal is clicked
-      vm.userDetails = {};
-      vm.userImageKtp = "";
-      vm.userImageProfile = "";
-      vm.userImageSelfieWithKtp = "";
-
-      vm.modalUserShow = true;
-      axios
-        .get(`${process.env.VUE_APP_API_URL}/users/${user.Hp}`, {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("auth_token")
-          }
-        })
-        .then(res => {
-          vm.userDetails = res.data[0];
-
-          // extract image
-          let parseImage = JSON.parse(res.data[0].ktp); // lol this, I need to parse???
-          for (let x in parseImage) {
-            if (x === "ktp") vm.userImageKtp = parseImage[x];
-            if (x === "photo") vm.userImageProfile = parseImage[x];
-          }
-
-          // extract value for pekerjaan
-          let pekerjaanValue = "";
-          switch (res.data[0].pekerjaan) {
-            case "negeri":
-              pekerjaanValue = "Pegawai Negeri Sipil";
-              break;
-            case "swasta":
-              pekerjaanValue = "Karyawan Swasta";
-              break;
-            case "bumn":
-              pekerjaanValue = "Karyawan BUMN";
-              break;
-            case "bumd":
-              pekerjaanValue = "Karyawan BUMD";
-              break;
-            case "konstruksi":
-              pekerjaanValue = "Karyawan Jasa Konstruksi";
-              break;
-            case "POLRI":
-              pekerjaanValue = "Kepolisian RI";
-              break;
-            case "TNI":
-              pekerjaanValue = "Tentara Nasional Indonesia";
-              break;
-            case "wiraswasta":
-              pekerjaanValue = "Wiraswasta";
-              break;
-            default:
-              alert("Pekerjaan not found!");
-              break;
-          }
-          vm.userDetailsPekerjaan = pekerjaanValue;
-
-          // extract value for penghasilan
-          let penghasilanValue = "";
-          switch (res.data[0].penghasilan) {
-            case "gol1":
-              penghasilanValue = "< Rp. 3.500.000";
-              break;
-            case "gol2":
-              penghasilanValue = "Rp. 3.500.000 - Rp 6.000.000";
-              break;
-            case "gol3":
-              penghasilanValue = "Rp. 6.000.000 - Rp 8.000.000";
-              break;
-            case "gol4":
-              penghasilanValue = "Rp. 8.000.000 - Rp 10.000.000";
-              break;
-            case "gol5":
-              penghasilanValue = "Rp. 10.000.000 - Rp 12.000.000";
-              break;
-            case "gol6":
-              penghasilanValue = "> Rp. 12.000.000";
-              break;
-            default:
-              alert("Penghasilan not found!");
-              break;
-          }
-          vm.userDetailsPenghasilan = penghasilanValue;
-        });
-    },
-
-    /**
-     * Activate user
-     *
-     * @param  int hp Phone Number
-     * @param  int index
-     */
-    activate(hp, index) {
-      let vm = this;
-      if (confirm("Activate this account?")) {
-        axios
-          .post(`${process.env.VUE_APP_API_URL}/users/${hp}/activated`, {
-            activated: 2
-          })
-          .then(res => {
-            alert("Successfully Activated!");
-            vm.users.splice(index, 1);
-          });
+      switch (modal) {
+        case "AddCategory":
+          vm.modalAddMerchantUser = true;
+          break;
+        default:
+          alert("error!");
       }
+    },
+    addMerchUser() {
+      let vm = this;
+      axios
+        .post(`${process.env.VUE_APP_API_URL}/merchant`, vm.dataAddMerchantUser)
+        .then(res => {
+          alert("Merchant User successfully added!");
+          axios
+            .get("https://sandbox2.empatkali.co.id/merchant")
+            .then(res2 => (vm.promotions = res2.data));
+        });
     }
   }
 };
 </script>
+
+<style scoped>
+</style>

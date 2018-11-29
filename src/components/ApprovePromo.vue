eb6a34735eded386206248b06fcf2fff31f3928a
Dhonald Johannes C.H. Sihotang <dhonaldjohannes@gmail.com>
2018-11-29 11:10:54 +0700

    Adding Some Flow in Promotion & Approve Promo Page

--- src/components/ApprovePromo.vue
+++ src/components/ApprovePromo.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
-    <h2>All Promotion at Merchant EmpatKali</h2>
+    <h2>Approve Promotion at Merchant EmpatKali</h2>
     <table class="table table-hover table-striped">
       <thead>
         <tr>
-          <th>No</th>
+          <th>id</th>
+          <th>Product</th>
           <th>Title</th>
           <th>Author</th>
           <th>Period</th>
           <th>Diskon</th>
-          <th>Detail</th>
           <th></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(data, index) in allPromotion">
+        <tr 
+          v-for="(data, index) in allPromotion" 
+          v-if="data.ok == 0"
+        >
           <td>{{ data.No }}</td>
+          <td style="width: 35%"><img :src="data.img" style="height: 230px; width: 70%; object-fit: cover" /></td>          
           <td style="width: 20%">{{ data.title }}</td>
           <td>{{ data.author }}</td>
           <td style="width: 17%">{{ data.period }}</td>
           <td>{{ data.diskon }}%</td>
-          <td style="width: 43%">{{ data.detail }}</td>
           <td class="text-center">Approve</td>
         </tr>
         <tr v-if="allPromotion.length === 0">
-          <td colspan="4">No promotion record!</td>
+          <td colspan="">No promotion record!</td>
         </tr>
       </tbody>
     </table>
--- src/components/AssignCategory.vue
+++ src/components/AssignCategory.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
-    <h2>All Promotion at Merchant EmpatKali</h2>
+    <h2>Assign Category to Merchant EmpatKali</h2>
     <table class="table table-hover table-striped">
       <thead>
         <tr>
           <th>No</th>
+          <th>Product</th>
           <th>Title</th>
           <th>Author</th>
           <th>Period</th>
           <th>Diskon</th>
-          <th>Detail</th>
           <th></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(data, index) in allPromotion">
-          <td>{{ data.No }}</td>
+          <td style="width: 5%">{{ data.No }}</td>
+          <td style="width: 35%"><img :src="data.img" style="height: 230px; width: 70%; object-fit: cover" /></td>
           <td style="width: 20%">{{ data.title }}</td>
-          <td>{{ data.author }}</td>
+          <td style="width: 10%">{{ data.author }}</td>
           <td style="width: 20%">{{ data.period }}</td>
-          <td>{{ data.diskon }}%</td>
-          <td>{{ data.detail }}</td>
-          <td v-if="data.ok === 1">selesai</td>
+          <td style="width: 10%">{{ data.diskon }}%</td>
         </tr>
         <tr v-if="allPromotion.length === 0">
           <td colspan="4">No transaction record!</td>
